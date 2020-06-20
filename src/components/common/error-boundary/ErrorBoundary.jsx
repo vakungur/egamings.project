d0b7ae16ce0b4ad76fe5b922a4e59999fbdc1beb
vakungur <vakungur@gmail.com>
2020-06-20 19:00:12 +0300

    final

---
+++ src/components/common/error-boundary/ErrorBoundary.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {ErrorIndicator} from "../index";
+
+export default class ErrorBoundary extends React.Component {
+
+    state = {
+        hasError: false
+    };
+
+    componentDidCatch() {
+        this.setState({hasError: true});
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <ErrorIndicator/>;
+        }
+        return this.props.children;
+    }
